7a4525db18f35af128e5d6a8c56662c8950f62c2
marinalytics <vivek@marinalytics.com>
2021-11-01 22:03:15 +0530

    Controlled Forms

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderMenuItem ({dish, onClick}) {
+function RenderMenuItem ({dish}) {
     return (
         <Card>
             <Link to={`/menu/${dish.id}`} >
@@ -16,33 +16,33 @@ function RenderMenuItem ({dish, onClick}) {
     );
 }
 
-    const Menu = (props) => {
+const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
-                </div>
-            );
-        });
-
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Menu</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    {menu}
-                </div>
+            <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                <RenderMenuItem dish={dish} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
+}
 
 export default Menu;
